c0f694d187dc0c13f98e079352cdd234fbd19c12
Stann11 <20it122@charusat.edu.in>
2023-05-29 12:04:03 +0530

    Adding new element to html using JS (naive)

---
+++ 113.js
@@ -0,0 +1,8 @@
+//Add new html element to html
+
+const todo=document.querySelector(".todo-list");
+console.log(todo);
+todo.innerHTML+="<li>Hello i am under the water</li>";
+todo.innerHTML+="<li>Here too much raining uuuuuuuu</li>"
+
+//This is not the most efficient way to create or add the elements to the HTML file
