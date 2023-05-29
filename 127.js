5a3d21fe33fde857227471ff7ee8f671966d4534
Stann11 <20it122@charusat.edu.in>
2023-05-29 16:56:30 +0530

    Keypress event and mouse over event

---
+++ 127.js
@@ -0,0 +1,14 @@
+// Keypress event 
+// Mouse over event 
+
+// const body1 = document.body;
+
+// body1.addEventListener("keypress", (e)=>{
+//     console.log(e.key);
+// });
+
+const button1 = document.querySelector(".btn-headline");
+
+button1.addEventListener("mouseover", ()=>{
+    console.log("Mouse over event occured");
+});
