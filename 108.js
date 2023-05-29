8fa4e50dbcb0e32d424d0d9d9fda5cf30b375ba8
Stann11 <20it122@charusat.edu.in>
2023-05-29 12:02:05 +0530

    Accessing multiple elements using getelementbyclass and queryselector all

---
+++ 108.js
@@ -0,0 +1,8 @@
+//get multiple elements by get elements by class name
+//get multiple elements item using queryselectorall
+
+const a = document.getElementsByClassName("nav-items"); //HTML Collection is obtained which is type of object
+console.log(a);
+
+const b = document.querySelectorAll(".nav-items"); //Node list is obtained which is type of object 
+console.log(b);
