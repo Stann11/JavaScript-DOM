ba2030e9e48ea7d134a13a6744587dae7a255010
Stann11 <20it122@charusat.edu.in>
2023-05-29 12:00:51 +0530

    Changing the style of the element using JS

---
+++ 106.js
@@ -0,0 +1,7 @@
+//Change the style of the elements 
+
+const hello = document.querySelector("section.sectiontodo h2");
+hello.style.color="Blue";
+hello.style.backgroundColor="Black";
+hello.style.border="20px solid green";
+console.log(hello.style);
