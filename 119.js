39dd767e2397147d8cd5766b0edcd282ab835e93
Stann11 <20it122@charusat.edu.in>
2023-05-29 15:21:39 +0530

    Get dimensions of the elements

---
+++ 119.js
@@ -0,0 +1,5 @@
+// How to get the dimension of element 
+// get height and width of element 
+
+const sectiontodo = document.querySelector(".sectiontodo");
+console.log(sectiontodo.getBoundingClientRect());
